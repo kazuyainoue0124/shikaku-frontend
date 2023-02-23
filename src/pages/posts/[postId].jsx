29b0388c8705e29a1135875f90d3e003e3ae21a9
kazuyainoue0124 <bauer24tbp@gmail.com>
2023-02-24 01:48:16 +0900

    getStaticPathsのfallbackをfalseに変更

--- src/pages/posts/[postId].jsx
+++ src/pages/posts/[postId].jsx
@@ -80,7 +80,7 @@ export const getStaticPaths = async () => {
 
   // path:事前ビルドするパスの対象を指定するパラメータ
   // fallback:事前ビルドしたパス以外にアクセスした時のパラメータ true:カスタム404pageを表示 false:404pageを表示
-  return { paths, fallback: true };
+  return { paths, fallback: false };
 };
 
 // paramsには上記pathsで指定した値が1postずつ入る
